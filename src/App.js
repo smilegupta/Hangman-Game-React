fe6a66f0d6d0228322165eb6e75ec1f753f128ad
smilegupta <guptamiley3012@gmail.com>
2020-09-13 02:53:28 +0530

    minimum game

--- src/App.js
+++ src/App.js
@@ -1,8 +1,56 @@
-import React from "react";
+import React, { Fragment, useState, useEffect } from "react";
+import Header from "./components/Header";
+import Figure from "./components/Figure";
+import WrongLetters from "./components/WrongLetters";
+import Word from "./components/Word";
+import Popup from "./components/Popup";
+import Notification from "./components/Notification";
 import "./App.css";
 
+const words = ["application", "programming", "interface", "wizard"];
+
+let selectedWord = words[Math.floor(Math.random() * words.length)];
+
 function App() {
-  return <div></div>;
+  const [playable, setPlayable] = useState(true);
+  const [correctLetters, setCorrectLetters] = useState([]);
+  const [wrongLetters, setWrongLetters] = useState([]);
+
+  useEffect(() => {
+    const handleKeydown = (event) => {
+      const { key, keyCode } = event;
+      if (playable && keyCode >= 65 && keyCode <= 90) {
+        const letter = key.toLowerCase();
+
+        if (selectedWord.includes(letter)) {
+          if (!correctLetters.includes(letter)) {
+            setCorrectLetters((currentLetters) => [...currentLetters, letter]);
+          } else {
+            //show(setShowNotification);
+          }
+        } else {
+          if (!wrongLetters.includes(letter)) {
+            setWrongLetters((currentLetters) => [...currentLetters, letter]);
+          } else {
+            // show(setShowNotification);
+          }
+        }
+      }
+    };
+    window.addEventListener("keydown", handleKeydown);
+    return () => window.removeEventListener("keydown", handleKeydown);
+  }, [correctLetters, wrongLetters, playable]);
+
+  return (
+    <>
+      <Header />
+      <div className="game-container">
+        <Figure />
+        <WrongLetters wrongLetters={wrongLetters} />
+        <Word selectedWord={selectedWord} correctLetters={correctLetters} />
+      </div>
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from "react";
+
+const Header = () => {
+  return (
+    <Fragment>
+      <h1>Hangman</h1>
+      <p>Find the hidden word - Enter a letter</p>
+    </Fragment>
+  );
+};
+
+export default Header;
